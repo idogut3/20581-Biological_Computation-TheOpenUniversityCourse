34ba5e1b6082e291688c8f82a0601a75c967982d
idogut3 <95476354+idogut3@users.noreply.github.com>
2024-12-25 21:07:01 +0200

    Add files via upload

---
+++ mmn12/SimulationConstants.py
@@ -0,0 +1,20 @@
+# Constants
+WIDTH, HEIGHT = 800, 600  # Size of the window
+CELL_SIZE = 10  # Size of each cell in the grid
+MS_PER_FRAME_DEFAULT = 100  # Frames per second (speed of the game)
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (169, 169, 169)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+PINK = (255,20,147)
+
+# Grid dimensions (based on window size and cell size)
+GRID_WIDTH = WIDTH // CELL_SIZE
+GRID_HEIGHT = HEIGHT // CELL_SIZE
+
+
+
